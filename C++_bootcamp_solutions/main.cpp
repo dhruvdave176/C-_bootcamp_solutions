cdd765ce55bce327ec15fc4f2799ca2e817ff1d5
Dhruv Dave <dhruvdave176@gmail.com>
2022-07-31 20:09:28 +0530

    Initial Commit

---
+++ C++_bootcamp_solutions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++_bootcamp_solutions
+//
+//  Created by Dhruv Dave on 31/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
